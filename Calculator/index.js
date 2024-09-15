35dc8868ce315f3773e66ad6ac349fa81067837f
Aniket_Kumar29 <148937741+AniketKumar2729@users.noreply.github.com>
2024-09-15 09:59:51 +0530

    Add files via upload

---
+++ Calculator/index.js
@@ -0,0 +1,12 @@
+console.log("woriking");
+let allbutton=$(".btn");
+console.log(allbutton);
+let result = 0;
+let prevEntry = 0;
+let operation = null;
+let currentEntry = "0";
+// updateScreen(result);
+$(".btn").click((evt)=>{
+  let buttonPressed = $(this).html();
+  console.log(buttonPressed);
+})
